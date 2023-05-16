6e83bbd336a565892a69c2fc1abbc752dc8a9fba
Nisargkumar Panchal <panchal.nisargkumar@gmail.com>
2023-05-16 16:58:50 +0200

    init visual odometry

---
+++ src/visual_odometry/scripts/visual_odometry_node.py
@@ -0,0 +1,311 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Header
+
+import os
+import numpy as np
+import cv2
+
+
+def matrix_to_quaternion(matrix):
+    trace = matrix[0, 0] + matrix[1, 1] + matrix[2, 2]
+    
+    if trace > 0:
+        S = np.sqrt(trace + 1.0) * 2.0
+        qw = 0.25 * S
+        qx = (matrix[2, 1] - matrix[1, 2]) / S
+        qy = (matrix[0, 2] - matrix[2, 0]) / S
+        qz = (matrix[1, 0] - matrix[0, 1]) / S
+    elif matrix[0, 0] > matrix[1, 1] and matrix[0, 0] > matrix[2, 2]:
+        S = np.sqrt(1.0 + matrix[0, 0] - matrix[1, 1] - matrix[2, 2]) * 2.0
+        qw = (matrix[2, 1] - matrix[1, 2]) / S
+        qx = 0.25 * S
+        qy = (matrix[0, 1] + matrix[1, 0]) / S
+        qz = (matrix[0, 2] + matrix[2, 0]) / S
+    elif matrix[1, 1] > matrix[2, 2]:
+        S = np.sqrt(1.0 + matrix[1, 1] - matrix[0, 0] - matrix[2, 2]) * 2.0
+        qw = (matrix[0, 2] - matrix[2, 0]) / S
+        qx = (matrix[0, 1] + matrix[1, 0]) / S
+        qy = 0.25 * S
+        qz = (matrix[1, 2] + matrix[2, 1]) / S
+    else:
+        S = np.sqrt(1.0 + matrix[2, 2] - matrix[0, 0] - matrix[1, 1]) * 2.0
+        qw = (matrix[1, 0] - matrix[0, 1]) / S
+        qx = (matrix[0, 2] + matrix[2, 0]) / S
+        qy = (matrix[1, 2] + matrix[2, 1]) / S
+        qz = 0.25 * S
+
+    return np.array([qx, qy, qz, qw])
+
+class CameraPoses():
+    
+    def __init__(self, data_dir, skip_frames, intrinsic):
+        
+        self.K = intrinsic
+        self.extrinsic = np.array(((1,0,0,0),(0,1,0,0),(0,0,1,0)))
+        self.P = self.K @ self.extrinsic
+        self.orb = cv2.ORB_create(3000)
+        FLANN_INDEX_LSH = 6
+        index_params = dict(algorithm=FLANN_INDEX_LSH, table_number=6, key_size=12, multi_probe_level=1)
+        search_params = dict(checks=50)
+        self.flann = cv2.FlannBasedMatcher(indexParams=index_params, searchParams=search_params)
+        
+        self.world_points = []
+
+        self.current_pose = None
+
+    @staticmethod
+    def _form_transf(R, t):
+        
+        T = np.eye(4, dtype=np.float64)
+        T[:3, :3] = R
+        T[:3, 3] = t
+        
+        return T
+
+    def get_world_points(self):
+        return np.array(self.world_points)
+    
+    def get_matches(self, img1, img2):
+   
+        # Find the keypoints and descriptors with ORB
+        kp1, des1 = self.orb.detectAndCompute(img1, None)
+        kp2, des2 = self.orb.detectAndCompute(img2, None)
+        # Find matches
+        if len(kp1) > 6 and len(kp2) > 6:
+            matches = self.flann.knnMatch(des1, des2, k=2)
+
+            # Find the matches there do not have a to high distance
+            good_matches = []
+            try:
+                for m, n in matches:
+                    if m.distance < 0.5 * n.distance:
+                        good_matches.append(m)
+            except ValueError:
+                pass
+            
+            # Draw matches
+            img_matches = np.empty((max(img1.shape[0], img2.shape[0]), img1.shape[1] + img2.shape[1], 3), dtype=np.uint8)
+            
+            # Get the image points form the good matches
+            q1 = np.float32([kp1[m.queryIdx].pt for m in good_matches])
+            q2 = np.float32([kp2[m.trainIdx].pt for m in good_matches])
+        
+            return q1, q2
+        else:
+            return None, None
+
+    def get_pose(self, q1, q2):
+    
+        # Essential matrix
+        E, mask = cv2.findEssentialMat(q1, q2, self.K)
+
+        # Decompose the Essential matrix into R and t
+        R, t = self.decomp_essential_mat_old(E, q1, q2)
+
+        # Get transformation matrix
+        transformation_matrix = self._form_transf(R, np.squeeze(t))
+        
+        return transformation_matrix
+
+
+    def decomp_essential_mat(self, E, q1, q2):
+
+        R1, R2, t = cv2.decomposeEssentialMat(E)
+        T1 = self._form_transf(R1,np.ndarray.flatten(t))
+        T2 = self._form_transf(R2,np.ndarray.flatten(t))
+        T3 = self._form_transf(R1,np.ndarray.flatten(-t))
+        T4 = self._form_transf(R2,np.ndarray.flatten(-t))
+        transformations = [T1, T2, T3, T4]
+        
+        # Homogenize K
+        K = np.concatenate((self.K, np.zeros((3,1)) ), axis = 1)
+
+        # List of projections
+        projections = [K @ T1, K @ T2, K @ T3, K @ T4]
+
+        np.set_printoptions(suppress=True)
+
+        positives = []
+        for P, T in zip(projections, transformations):
+            hom_Q1 = cv2.triangulatePoints(P, P, q1.T, q2.T)
+            hom_Q2 = T @ hom_Q1
+            # Un-homogenize
+            Q1 = hom_Q1[:3, :] / hom_Q1[3, :]
+            Q2 = hom_Q2[:3, :] / hom_Q2[3, :]
+             
+            total_sum = sum(Q2[2, :] > 0) + sum(Q1[2, :] > 0)
+            relative_scale = np.mean(np.linalg.norm(Q1.T[:-1] - Q1.T[1:], axis=-1)/
+                                     np.linalg.norm(Q2.T[:-1] - Q2.T[1:], axis=-1))
+            positives.append(total_sum + relative_scale)
+            
+
+        # Decompose the Essential matrix using built in OpenCV function
+        # Form the 4 possible transformation matrix T from R1, R2, and t
+        # Create projection matrix using each T, and triangulate points hom_Q1
+        # Transform hom_Q1 to second camera using T to create hom_Q2
+        # Count how many points in hom_Q1 and hom_Q2 with positive z value
+        # Return R and t pair which resulted in the most points with positive z
+
+        max = np.argmax(positives)
+        if (max == 2):
+            return R1, np.ndarray.flatten(-t)
+        elif (max == 3):
+            return R2, np.ndarray.flatten(-t)
+        elif (max == 0):
+            return R1, np.ndarray.flatten(t)
+        elif (max == 1):
+            return R2, np.ndarray.flatten(t)
+        
+        
+    def decomp_essential_mat_old(self, E, q1, q2):
+        def sum_z_cal_relative_scale(R, t):
+            # Get the transformation matrix
+            T = self._form_transf(R, t)
+            # Make the projection matrix
+            P = np.matmul(np.concatenate((self.K, np.zeros((3, 1))), axis=1), T)
+
+            # Triangulate the 3D points
+            hom_Q1 = cv2.triangulatePoints(self.P, P, q1.T, q2.T)
+            # Also seen from cam 2
+            hom_Q2 = np.matmul(T, hom_Q1)
+
+            # Un-homogenize
+            Q1 = hom_Q1[:3, :] / hom_Q1[3, :]
+            Q2 = hom_Q2[:3, :] / hom_Q2[3, :]
+            
+            #self.world_points.append(Q1)
+
+            # Find the number of points there has positive z coordinate in both cameras
+            sum_of_pos_z_Q1 = sum(Q1[2, :] > 0)
+            sum_of_pos_z_Q2 = sum(Q2[2, :] > 0)
+
+            # Form point pairs and calculate the relative scale
+            relative_scale = np.mean(np.linalg.norm(Q1.T[:-1] - Q1.T[1:], axis=-1)/
+                                     np.linalg.norm(Q2.T[:-1] - Q2.T[1:], axis=-1))
+            return sum_of_pos_z_Q1 + sum_of_pos_z_Q2, relative_scale
+
+        # Decompose the essential matrix
+        R1, R2, t = cv2.decomposeEssentialMat(E)
+        t = np.squeeze(t)
+
+        # Make a list of the different possible pairs
+        pairs = [[R1, t], [R1, -t], [R2, t], [R2, -t]]
+
+        # Check which solution there is the right one
+        z_sums = []
+        relative_scales = []
+        for R, t in pairs:
+            z_sum, scale = sum_z_cal_relative_scale(R, t)
+            z_sums.append(z_sum)
+            relative_scales.append(scale)
+
+        # Select the pair there has the most points with positive z coordinate
+        right_pair_idx = np.argmax(z_sums)
+        right_pair = pairs[right_pair_idx]
+        relative_scale = relative_scales[right_pair_idx]
+        R1, t = right_pair
+        t = t * relative_scale
+        t[~np.isfinite(t)] = 0.00001
+        T = self._form_transf(R1, t)
+        # Make the projection matrix
+        P = np.matmul(np.concatenate((self.K, np.zeros((3, 1))), axis=1), T)
+        # Triangulate the 3D points
+        hom_Q1 = cv2.triangulatePoints(P, P, q1.T, q2.T)
+        # Also seen from cam 2
+        hom_Q2 = np.matmul(T, hom_Q1)
+
+        # Un-homogenize
+        Q1 = hom_Q1[:3, :] / hom_Q1[3, :]
+        Q2 = hom_Q2[:3, :] / hom_Q2[3, :]
+        
+        self.world_points.append(Q1)
+
+        return [R1, t]
+
+
+if __name__ == "__main__":
+
+    intrinsic = np.load('/home/zeelpatel/Desktop/intrinsicNew.npy')
+    
+    rospy.init_node("visual_odometry_node")
+
+    vo_pub = rospy.Publisher("/visual_odom", PoseStamped, queue_size=10)
+    
+    skip_frames = 2
+    data_dir = ''
+    vo = CameraPoses(data_dir, skip_frames, intrinsic)
+    odometry = PoseStamped()
+    estimated_path = []
+    camera_pose_list = []
+    start_pose = np.ones((3,4))
+    start_translation = np.zeros((3,1))
+    start_rotation = np.identity(3)
+    start_pose = np.concatenate((start_rotation, start_translation), axis=1)
+
+    try:
+        cap = cv2.VideoCapture(0)
+        cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+        rospy.loginfo("Camera opened")
+    # Check if camera opened successfully
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Error opening video stream")
+    
+    
+
+    process_frames = False
+    old_frame = None
+    new_frame = None
+    frame_counter = 0
+
+    cur_pose = start_pose
+    
+    while(cap.isOpened() and not rospy.is_shutdown()):
+
+        # Capture frame-by-frame
+        ret, new_frame = cap.read()
+
+        frame_counter += 1
+
+        if process_frames and ret:
+            q1, q2 = vo.get_matches(old_frame, new_frame)
+            if q1 is not None:
+                if (len(q1) > 20 and len(q2) > 20) and (q1.all() == q2.all()):
+                    transf = vo.get_pose(q1, q2)
+                    cur_pose = np.dot(cur_pose,transf)
+
+            hom_array = np.array([[0,0,0,1]])
+            hom_camera_pose = np.concatenate((cur_pose,hom_array), axis=0)
+            camera_pose_list.append(hom_camera_pose)
+            estimated_path.append((cur_pose[0, 3], cur_pose[2, 3]))
+
+            estimated_camera_pose_x, estimated_camera_pose_y = cur_pose[0, 3], cur_pose[2, 3]
+
+        elif process_frames and ret is False:
+            break
+        
+        old_frame = new_frame
+
+        process_frames = True
+        current_time = rospy.Time.now()
+
+        odometry.header = Header()
+        odometry.header.stamp = current_time
+        odometry.header.frame_id = "base_link"
+        odometry.pose.position.x = cur_pose[0,3]
+        odometry.pose.position.y = cur_pose[1,3]
+        odometry.pose.position.z = cur_pose[2,3]
+
+        cur_quat = matrix_to_quaternion(cur_pose[:, 0:3])
+
+        # Set the orientation if available
+        odometry.pose.orientation.x = cur_quat[0]
+        odometry.pose.orientation.y = cur_quat[1]
+        odometry.pose.orientation.z = cur_quat[2]
+        odometry.pose.orientation.w = cur_quat[3]
+        
+
+    # When everything done, release the video capture object
+    cap.release()
